a2d754d40cc55ec835f0fbb631101854051d6b25
Sahil Kumar <107185166+Krsahil-07@users.noreply.github.com>
2024-04-20 01:06:00 +0530

    Time: 46 ms (62.11%) | Memory: 16.5 MB (90.62%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
@@ -1,8 +1,16 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
+        # ans=0
+        # for i in range(len(nums)):
+        #     if nums[i]<k:
+        #         ans+=1
+        # return ans
         ans=0
-        for i in range(len(nums)):
-            if nums[i]<k:
+        heapify(nums)
+        while 1:
+            m=heappop(nums)
+            if m<k:
                 ans+=1
+            else:
+                break
         return ans
-        
